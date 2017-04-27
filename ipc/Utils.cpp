61c38344497af7b28c10d4bdc6e4fb9703c0f888
MatheusMS01 <Matheus.M.Sarmento@gmail.com>
2017-04-27 15:46:53 -0300

    Update Utils.cpp

--- ipc/Utils.cpp
+++ ipc/Utils.cpp
@@ -20,7 +20,7 @@ unsigned int Utils::distanceBetweenNodes(int nodeId1, int nodeId2)
    int x2 = nodeId2 % 4;
    int y2 = nodeId2 / 4;
 
-   const auto distance = static_cast<unsigned int>(std::abs(static_cast<long>(x2 - x1)) + std::abs(static_cast<long>(y2 - y1)));
+   const auto distance = static_cast<unsigned int>(std::abs(static_cast<double>(x2 - x1)) + std::abs(static_cast<double>(y2 - y1)));
 
    return distance;
 }
