fd0fbc5cc2ee404ff7e4e2acdaa7b0cc5deff711
MatheusMS01 <Matheus.M.Sarmento@gmail.com>
2017-07-11 14:02:45 -0300

    Create Field.h

---
+++ ipc/Field.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define FIELD( name, type ) \
+   private: \
+      type name; \
+   public: \
+      const type& Get##name() const { return name; }; \
+      void Set##name( const type& value ) { name = value; };
